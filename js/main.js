438f787aead2af9992637357dbe7d9556903eeb2
shelbyboyd <shelbyboyd@Shelbys-MacBook-Pro-2.local>
2015-03-04 16:55:43 -0500

    added js.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 var $form = $('form');
-var $input = $('#list');
+var $input = $('#items');
+var $theList = $('.list');
+var $li = $('<li>');
 
-
-$form.on('submit', function(eventObject) {
-
-    var $theH2 = $('<h2>');
-    $theH2.html($input.val());
-    $input.prepend($theH2);
+$form.on('submit', function (eventObject) {
+    eventObject.preventDefault();
+    
+    
+    var $li = $('<li>');
+    $li.html($input.val());
+    
+    var $inputRemove = $('<button class="remove x-button">x</button>');
+    
+    $inputRemove.on('click', function () {
+        $li.remove('li');
+    });
+    
+    $li.click(function(){
+        $(this).addClass('done');
+    });
+    
+    $li.append($inputRemove);
+    $theList.prepend($li);
     $input.val('');
 });
+
+
